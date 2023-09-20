dd63b12bd83d7880b5c87325e8ff8cb53774cdd0
Aldio Lisafron <lisafronaldio123@gmail.com>
2023-09-21 01:44:57 +0700

    Finalize country resource read API

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -15,72 +15,13 @@ class CountryController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $country = Country::get();
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCountryRequest  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(StoreCountryRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Country  $country
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Country $country)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Country  $country
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Country $country)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCountryRequest  $request
-     * @param  \App\Models\Country  $country
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateCountryRequest $request, Country $country)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Country  $country
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Country $country)
-    {
-        //
+        return response()->json([
+            'message' => __('api.read.success', ['model' => __('countries')]),
+            'data' => [
+                'country' => $country
+            ]
+        ]);
     }
 }
